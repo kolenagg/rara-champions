26211a3e0cee9c30a8e7f6281d20e92ca7ab98d4
Kolena <154228183+kolenagg@users.noreply.github.com>
2023-12-18 13:59:52 +0100

    feat: add energy label bg

--- deck.rb
+++ deck.rb
@@ -5,13 +5,16 @@ card_height = 585      # 4096 / 7
 
 Squib::Deck.new(
   cards: 3,
+  dpi: 72,
   width: card_width,
   height: card_height,
   layout: 'layout.yml',
 ) do
   background color: :white
-
+  set font: 'Inter 18'
   rect layout: 'background'
+
+  circle layout: 'energy_label_bg'
 
   save format: :png
   # save_sheet columns: 10, rows: 7, prefix: 'deck_'
