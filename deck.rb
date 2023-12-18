085b2402d74834c34a532f877b1d75c55ac3d100
Kolena <154228183+kolenagg@users.noreply.github.com>
2023-12-18 13:26:05 +0100

    feat: sheet for tts export

--- deck.rb
+++ deck.rb
@@ -1,6 +1,11 @@
 require 'squib'
 
+card_width = 409       # 4096 / 10
+card_height = 585     # 4096 / 7
+
 Squib::Deck.new(cards: 3, layout: 'layout.yml') do
+  background color: :white
   text str: 'Hello, World!'
-  save format: :png
+  # save format: :png
+  save_sheet columns: 10, rows: 7, prefix: 'deck_'
 end
