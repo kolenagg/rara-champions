6e762cd889c0c0a98879f9b3e4bf02d2d791e031
Kolena <154228183+kolenagg@users.noreply.github.com>
2023-12-18 13:49:31 +0100

    feat: add background rect

--- deck.rb
+++ deck.rb
@@ -1,11 +1,18 @@
 require 'squib'
 
 card_width = 409       # 4096 / 10
-card_height = 585     # 4096 / 7
+card_height = 585      # 4096 / 7
 
-Squib::Deck.new(cards: 3, layout: 'layout.yml') do
+Squib::Deck.new(
+  cards: 3,
+  width: card_width,
+  height: card_height,
+  layout: 'layout.yml',
+) do
   background color: :white
-  text str: 'Hello, World!'
-  # save format: :png
-  save_sheet columns: 10, rows: 7, prefix: 'deck_'
+
+  rect layout: 'background'
+
+  save format: :png
+  # save_sheet columns: 10, rows: 7, prefix: 'deck_'
 end
